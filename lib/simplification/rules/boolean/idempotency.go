d225492cf22e82504f73b491b38a68ce9aa8aaae
Ademun <78663343+Ademun@users.noreply.github.com>
2025-09-15 00:49:07 +0300

    feat: idempotency rule

---
+++ lib/simplification/rules/boolean/idempotency.go
@@ -0,0 +1,36 @@
+package boolean
+
+import (
+	"logicka/lib/ast"
+	"logicka/lib/lexer"
+	"logicka/lib/simplification/rules/base"
+)
+
+type IdempotencyRule struct {
+	base.BaseRule
+}
+
+func NewIdempotencyRule() *IdempotencyRule {
+	return &IdempotencyRule{
+		BaseRule: *base.NewBaseRule("Idempotency law"),
+	}
+}
+
+func (r *IdempotencyRule) CanApply(node ast.ASTNode) bool {
+	binary, ok := node.(*ast.BinaryNode)
+	if !ok {
+		return false
+	}
+
+	return binary.Operator == lexer.CONJ || binary.Operator == lexer.DISJ
+}
+
+func (r *IdempotencyRule) Apply(node ast.ASTNode) (ast.ASTNode, error) {
+	binary := node.(*ast.BinaryNode)
+
+	if binary.Left.Equals(binary.Right) {
+		return binary.Left, nil
+	}
+
+	return node, nil
+}
