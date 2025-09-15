4cb4116432fd95d1b0aaa0f02c3743a51404001b
Ademun <78663343+Ademun@users.noreply.github.com>
2025-09-16 01:03:28 +0300

    feat: equivalence rule

---
+++ lib/simplification/rules/boolean/equivalence.go
@@ -0,0 +1,36 @@
+package boolean
+
+import (
+	"logicka/lib/ast"
+	"logicka/lib/lexer"
+	"logicka/lib/simplification/rules/base"
+)
+
+type EquivalenceRule struct {
+	base.BaseRule
+}
+
+func NewEquivalenceRule() *EquivalenceRule {
+	return &EquivalenceRule{
+		BaseRule: *base.NewBaseRule("Equivalence law"),
+	}
+}
+
+func (r *EquivalenceRule) CanApply(node ast.ASTNode) bool {
+	binary, ok := node.(*ast.BinaryNode)
+	if !ok {
+		return false
+	}
+
+	return binary.Operator == lexer.EQUIV
+}
+
+func (r *EquivalenceRule) Apply(node ast.ASTNode) (ast.ASTNode, error) {
+	binary := node.(*ast.BinaryNode)
+
+	return ast.NewBinaryNode(
+		lexer.CONJ,
+		ast.NewBinaryNode(lexer.IMPL, binary.Left, binary.Right),
+		ast.NewBinaryNode(lexer.IMPL, binary.Right, binary.Left),
+	), nil
+}
