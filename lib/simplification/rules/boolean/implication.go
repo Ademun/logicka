94a1221f669fcd993ed510290a4b3fb56ebbdf69
Ademun <78663343+Ademun@users.noreply.github.com>
2025-09-16 00:53:38 +0300

    feat: implication rule

---
+++ lib/simplification/rules/boolean/implication.go
@@ -0,0 +1,32 @@
+package boolean
+
+import (
+	"logicka/lib/ast"
+	"logicka/lib/lexer"
+	"logicka/lib/simplification/rules/base"
+)
+
+type ImplicationRule struct {
+	base.BaseRule
+}
+
+func NewImplicationRule() *ImplicationRule {
+	return &ImplicationRule{
+		BaseRule: *base.NewBaseRule("Implication law"),
+	}
+}
+
+func (r *ImplicationRule) CanApply(node ast.ASTNode) bool {
+	binary, ok := node.(*ast.BinaryNode)
+	if !ok {
+		return false
+	}
+
+	return binary.Operator == lexer.IMPL
+}
+
+func (r *ImplicationRule) Apply(node ast.ASTNode) (ast.ASTNode, error) {
+	binary := node.(*ast.BinaryNode)
+
+	return ast.NewBinaryNode(lexer.DISJ, ast.NewUnaryNode(lexer.NEG, binary.Left), binary.Right), nil
+}
