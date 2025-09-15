33ee5e7a7b4e3606ea963408725fe3214c2eb63c
Ademun <78663343+Ademun@users.noreply.github.com>
2025-09-16 00:42:24 +0300

    feat: doubleNegation.go negation rule

---
+++ lib/simplification/rules/boolean/doubleNegation.go
@@ -0,0 +1,36 @@
+package boolean
+
+import (
+	"logicka/lib/ast"
+	"logicka/lib/lexer"
+	"logicka/lib/simplification/rules/base"
+)
+
+type DoubleNegationRule struct {
+	base.BaseRule
+}
+
+func NewDoubleNegationRule() *DoubleNegationRule {
+	return &DoubleNegationRule{
+		BaseRule: *base.NewBaseRule("DoubleNegation law"),
+	}
+}
+
+func (r *DoubleNegationRule) CanApply(node ast.ASTNode) bool {
+	unary, ok := node.(*ast.UnaryNode)
+	if !ok {
+		return false
+	}
+
+	return unary.Operator == lexer.NEG
+}
+
+func (r *DoubleNegationRule) Apply(node ast.ASTNode) (ast.ASTNode, error) {
+	unary := node.(*ast.UnaryNode)
+
+	if negOp, ok := unary.Operand.(*ast.UnaryNode); ok && negOp.Operator == lexer.NEG {
+		return negOp.Operand, nil
+	}
+
+	return node, nil
+}
